d100933f3a7b54cee6e94a9488083348317a76fb
Szymon Rut <szymon.rut@airslate.com>
2025-02-28 11:15:54 +0100

    Add CLIP::ImageEmbedding

---
+++ app/models/clip/image_embedding.rb
@@ -0,0 +1,15 @@
+class CLIP::ImageEmbedding
+  def initialize(model_path: "model/visual.onnx", preprocessor: CLIP::ImagePreprocessor.new)
+    @model = OnnxRuntime::Model.new(model_path)
+    @preprocessor = preprocessor
+
+    def call(image_path)
+      image = preprocessor.preprocess(image_path).to_a
+      model.predict(input: { "input" => [ image ] })["output"].first
+    end
+
+    private
+
+    attr_reader :model, :preprocessor
+  end
+end
